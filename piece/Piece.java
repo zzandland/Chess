be27873c557ec521b01c58c15c26154785fc2649
Si Yong Kim <zzandland@gmail.com>
2019-02-24 20:41:43 -0800

    complete Tower movelogic with some refactoring to encapsulate implementation details

--- piece/Piece.java
+++ piece/Piece.java
@@ -14,4 +14,54 @@ public abstract class Piece {
   public char getRole() { return role; }
 
   public abstract boolean moveLogic(int fromCoord[], int toCoord[], Piece board[][]);
+
+  protected boolean perpedicularDashLogic(int fromCoord[], int toCoord[], Piece board[][]) {
+    if (fromCoord[0] == toCoord[0]) {
+      int horLimit[] = getHorLimit(fromCoord, board);
+      return (toCoord[1] >= horLimit[0] && toCoord[1] <= horLimit[1]);
+    } else if (fromCoord[1] == toCoord[1]) {
+      int verLimit[] = getVerLimit(fromCoord, board);
+      return (toCoord[0] >= verLimit[0] && toCoord[0] <= verLimit[1]);
+    }
+    return false;
+  }
+
+  private int[] getHorLimit(int fromCoord[], Piece board[][]) {
+    int left = 0;
+    int right = board[0].length;
+    for (int i = 0; i < board[fromCoord[0]].length; i++) {
+      Piece piece = board[fromCoord[0]][i];
+      if (piece != null) {
+        if (i < fromCoord[1] && i > left) {
+          if (piece.getSide() == getSide()) left = i + 1;
+          else if (piece.getSide() != getSide()) left = i;
+        } else if (i > fromCoord[1] && i < right) {
+          if (piece.getSide() == getSide()) right = i - 1;
+          else if (piece.getSide() != getSide()) right = i;
+        }
+      }
+    }
+    int pair[] = {left, right};
+    return pair;
+  }
+
+  private int[] getVerLimit(int fromCoord[], Piece board[][]) {
+    int top = 0;
+    int bottom = board.length;
+    for (int i = 0; i < board.length; i++) {
+      Piece piece = board[i][fromCoord[1]];
+      if (piece != null) {
+        if (i < fromCoord[0] && i > top) {
+          if (piece.getSide() == getSide()) top = i + 1;
+          else if (piece.getSide() != getSide()) top = i;
+        } else if (i > fromCoord[0] && i < bottom) {
+          if (piece.getSide() == getSide()) bottom = i - 1;
+          else if (piece.getSide() != getSide()) bottom = i;
+        }
+      }
+    }
+    int pair[] = {top, bottom};
+    return pair;
+  }
+
 }
--- piece/Tower.java
+++ piece/Tower.java
@@ -6,54 +6,7 @@ public class Tower extends Piece {
   public boolean moveLogic(int fromCoord[], int toCoord[], Piece board[][]) {
     if (board[toCoord[0]][toCoord[1]] == null 
       || board[toCoord[0]][toCoord[1]].getSide() != getSide()) {
-      if (fromCoord[0] == toCoord[0]) {
-        int horLimit[] = getHorLimit(fromCoord, board);
-        return (toCoord[1] >= horLimit[0] && toCoord[1] <= horLimit[1]);
-      } else if (fromCoord[1] == toCoord[1]) {
-        int verLimit[] = getVerLimit(fromCoord, board);
-        return (toCoord[0] >= verLimit[0] && toCoord[0] <= verLimit[1]);
-      }
     }
-    return false;
+    return perpedicularDashLogic(fromCoord, toCoord, board);
   }
-
-  private int[] getHorLimit(int fromCoord[], Piece board[][]) {
-    int left = 0;
-    int right = board[0].length;
-    for (int i = 0; i < board[fromCoord[0]].length; i++) {
-      Piece piece = board[fromCoord[0]][i];
-      if (piece != null) {
-        if (i < fromCoord[1] && i > left) {
-          if (piece.getSide() == getSide()) left = i + 1;
-          else if (piece.getSide() != getSide()) left = i;
-        } else if (i > fromCoord[1] && i < right) {
-          if (piece.getSide() == getSide()) right = i - 1;
-          else if (piece.getSide() != getSide()) right = i;
-        }
-      }
-    }
-    int pair[] = {left, right};
-    return pair;
-  }
-
-  private int[] getVerLimit(int fromCoord[], Piece board[][]) {
-    int top = 0;
-    int bottom = board.length;
-    for (int i = 0; i < board.length; i++) {
-      Piece piece = board[i][fromCoord[1]];
-      if (piece != null) {
-        if (i < fromCoord[0] && i > top) {
-          if (piece.getSide() == getSide()) top = i + 1;
-          else if (piece.getSide() != getSide()) top = i;
-        } else if (i > fromCoord[0] && i < bottom) {
-          if (piece.getSide() == getSide()) bottom = i - 1;
-          else if (piece.getSide() != getSide()) bottom = i;
-        }
-      }
-    }
-    int pair[] = {top, bottom};
-    return pair;
-  }
-
-  
 }
